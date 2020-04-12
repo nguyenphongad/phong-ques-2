fb5129606371ed01264b7f01c3f9f85b070c26f8
nguyenphongad <62506629+nguyenphongad@users.noreply.github.com>
2020-04-12 19:24:07 +0700

    Update bt3.php

--- bt3.php
+++ bt3.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Test</title>
+	<title>Bài 3</title>
 	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:700%7CNunito:300,600" rel="stylesheet"> 
 	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link type="text/css" rel="stylesheet" href="style.css"/>
-
 </head>
 <body>
 	<!-- Header -->
